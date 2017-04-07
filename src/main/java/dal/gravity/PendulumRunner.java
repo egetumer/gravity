564498668de3c7f2ea4e72361b175ff633b9d187
Ege Tumer <egetumer@gmail.com>
2017-04-06 23:38:35 -0300

    An instance of gravity change is implemented.

--- src/main/java/dal/gravity/PendulumRunner.java
+++ src/main/java/dal/gravity/PendulumRunner.java
@@ -12,18 +12,30 @@ public class PendulumRunner {
 	NumberFormat nf = NumberFormat.getInstance ();
 	nf.setMaximumFractionDigits (3);
 
-	double gEarth = 9.80665; // Earth's gravitational field
+	GravityConstant gEarth = new GravityConstant(9.80665);
+	GravityConstant gJupiter = new GravityConstant(24.79);
+	//double gEarth = 9.80665; // Earth's surface gravity
+	//double gJupiter = 24.79; // Jupiter's surface gravity
 	double delta = (args.length == 0) ? .1 : Double.parseDouble (args[0]);
 	double sLen = 10, pMass = 10, theta0 = Math.PI/30;
-	RegularPendulum rp = new RegularPendulum (sLen, pMass, theta0, new GravityConstant (gEarth), delta);
-	SimplePendulum sp = new SimplePendulum (sLen, pMass, theta0, new GravityConstant (gEarth));
-	RegularPendulum rpCoarse = new RegularPendulum (sLen, pMass, theta0, new GravityConstant (gEarth), .1);
+	RegularPendulum rp = new RegularPendulum (sLen, pMass, theta0, gEarth, delta);
+	SimplePendulum sp = new SimplePendulum (sLen, pMass, theta0, gEarth);
+	RegularPendulum rpCoarse = new RegularPendulum (sLen, pMass, theta0, gEarth, .1);
 
 	// print out difference in displacement in 1 second intervals
 	// for 20 seconds
 	int iterations = (int) (1/delta);
 	System.out.println ("analytical vs. numerical displacement (fine, coarse)");
-	for (int second = 1; second <= 20; second++) {
+	for (int second = 1; second <= 40; second++) {
+		if (second == 1) System.out.println ("with gravity of Earth...");
+		
+		if (second == 21) {
+			sp.setGravityModel(gJupiter);
+			rp.setGravityModel(gJupiter);
+			rpCoarse.setGravityModel(gJupiter);
+			System.out.println ("with gravity of Jupiter...");
+		}
+		
 	    for (int i = 0; i < iterations; i++) rp.step ();
 	    for (int i = 0; i < 10; i++) rpCoarse.step (); 
 	    System.out.println ("t=" + second + "s: \t" + 
